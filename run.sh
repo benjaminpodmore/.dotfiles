612c0e4be3f5bc403fd18fa10d4a313db607b0ef
Benjamin Podmore <46699356+benjaminpodmore@users.noreply.github.com>
2021-02-01 23:54:16 +0800

    Add dotfiles

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vim_runtime ~/.vim_runtime
