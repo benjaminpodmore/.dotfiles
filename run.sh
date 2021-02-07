94b9a7ea92be4614437f65a2886a3462fc590a48
Benjamin Podmore <46699356+benjaminpodmore@users.noreply.github.com>
2021-02-07 19:07:58 +0800

    Pi config

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ FILES=".gitconfig .zshrc .p10k.zsh .vimrc .oh-my-zsh .vim_runtime"
 
 for FILE in $FILES
 do
-    if [ -e "/home/benjamin/$FILE" ]
+    if [ -e "/home/pi/$FILE" ]
     then
         echo "$FILE exists"
     else
-        ln -s "/home/benjamin/.dotfiles/$FILE" "/home/benjamin/$FILE"
+        ln -s "/home/pi/.dotfiles/$FILE" "/home/pi/$FILE"
         echo "Linked $FILE to home directory"
     fi
 done
